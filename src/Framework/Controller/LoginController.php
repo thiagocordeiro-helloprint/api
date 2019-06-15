7b3dfb584e1724e7ecfc3d138554c11258aa9ce6
Thiago Cordeiro <thiago.cordeiro@check24.de>
2019-06-15 15:45:09 +0200

    return user on login

--- src/Framework/Controller/LoginController.php
+++ src/Framework/Controller/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Framework\Controller;
 use App\HelloPrint\Auth\Exception\ServiceUnavailableException;
 use App\HelloPrint\Auth\Exception\UserNotFoundException;
 use App\HelloPrint\Auth\LoginService;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -18,7 +19,7 @@ class LoginController
         $this->service = $service;
     }
 
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
         $body = json_decode($request->getContent(), true);
         $email = $body['email'] ?? '';
@@ -29,11 +30,16 @@ class LoginController
         }
 
         try {
-            $this->service->doLogin($email, $password);
+            $user = $this->service->doLogin($email, $password);
         } catch (UserNotFoundException $e) {
             throw new HttpException(Response::HTTP_NOT_FOUND);
         } catch (ServiceUnavailableException $e) {
             throw new HttpException(Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+
+        return new JsonResponse([
+            'username' => $user->getUsername(),
+            'email' => $user->getEmail(),
+        ]);
     }
 }
